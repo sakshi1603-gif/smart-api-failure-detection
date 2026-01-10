a18e5b657993a573f0fc533d6791d33a91c7a1d7
sakshi biradar <biradarsakshi8@gmail.com>
2026-01-10 18:01:14 +0530

    add failure detection logic

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,25 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const apiRoutes = require("./route/Api.routes");
-
+require("./cron/monitor.cron");
 dotenv.config();
 
 const app = express();
-const PORT  = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/apis", apiRoutes);
 
-module.exports = app;
+async function startServer() {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server", err);
+  }
+}
+
+
+startServer();
